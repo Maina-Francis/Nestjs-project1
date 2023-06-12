2a49989944200a1347be014916b0a00dd1b23a64
francis <francismainakabutha@gmail.com>
2023-06-12 11:03:47 +0300

    created signup service that returns the user token

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,4 +1,42 @@
 import { Injectable } from '@nestjs/common';
+import { InjectModel } from '@nestjs/mongoose';
+import { User } from './schemas/auth.user.schema';
+import { Model } from 'mongoose';
+import * as bcrypt from 'bcryptjs';
+import { JwtService } from '@nestjs/jwt';
 
 @Injectable()
-export class AuthService {}
+export class AuthService {
+  constructor(
+    @InjectModel(User.name)
+    private userModel: Model<User>,
+    private jwtService: JwtService,
+  ) {}
+
+  //   User Signup service
+  async signUp(signUpDTO: SignUpDTO): Promise<{ token: string }> {
+    const { firstName, lastName, email, password } = signUpDTO;
+
+    //hash password before saving to database
+    const hashedPassword = await bcrypt.hash(password, 10);
+
+    // Save User to database
+    const user = await this.userModel.create({
+      firstName,
+      lastName,
+      email,
+      password: hashedPassword,
+    });
+
+    // assign jwt token to user
+    const token = this.jwtService.sign({
+      // payload
+      id: user._id,
+      firsName: firstName,
+      lastName: lastName,
+      email: email,
+    });
+
+    return { token };
+  }
+}
