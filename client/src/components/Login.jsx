e2ab3d5e6f303a6f762121169e539a7d208a986d
francis <francismainakabutha@gmail.com>
2023-06-13 13:02:04 +0300

    added state management to the login form

--- client/src/components/Login.jsx
+++ client/src/components/Login.jsx
@@ -1,21 +1,43 @@
-import React from "react";
+import React, { useState } from "react";
 import { Link } from "react-router-dom";
+import Validation from "./LoginValidation";
 
 const Login = () => {
+  const [values, setValues] = useState({
+    email: "",
+    password: "",
+  });
+
+  const [errors, setErrors] = useState({});
+
+  const handleInput = (e) => {
+    setValues((prev) => ({ ...prev, [e.target.name]: [e.target.value] }));
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    setErrors(Validation(values));
+  };
+
   return (
     <div className="d-flex justify-content-center align-items-center bg-primary vh-100">
       <div className="bg-white p-3 rounded w-25">
         <h2>Login</h2>
-        <form action="">
+        <form action="" onSubmit={handleSubmit}>
           <div className="mb-3">
             <label htmlFor="email">
               <strong>Email</strong>
             </label>
             <input
               type="email"
+              name="email"
               placeholder="Enter Email"
+              onChange={handleInput}
               className="form-control rounded-1"
             />
+            {errors.email && (
+              <span className="text-danger">{errors.email}</span>
+            )}
           </div>
 
           <div className="mb-3">
@@ -25,11 +47,18 @@ const Login = () => {
             <input
               type="password"
               placeholder="Enter Password"
+              name="password"
+              onChange={handleInput}
               className="form-control rounded-1"
             />
+            {/* {errors.password && (
+              <span className="text-danger">{errors.password}</span>
+            )} */}
           </div>
 
-          <button className="btn btn-success w-100 rounded-1">Login</button>
+          <button type="submit" className="btn btn-success w-100 rounded-1">
+            Login
+          </button>
 
           <p>You agree to our terms and conditions</p>
           <Link
