b9265fa0e6cc52a3db696a1fe572b5aa116102e0
francis <francismainakabutha@gmail.com>
2023-06-13 13:40:18 +0300

    updated loginValidation to only validate email

--- client/src/components/SignUp.jsx
+++ client/src/components/SignUp.jsx
@@ -1,12 +1,31 @@
-import React from "react";
+import React, { useState } from "react";
 import { Link } from "react-router-dom";
+import Validation from "./LoginValidation";
 
 const SignUp = () => {
+  const [values, setValues] = useState({
+    firstName: "",
+    lastName: "",
+    userName: "",
+    email: "",
+    password: "",
+  });
+
+  const [errors, setErrors] = useState({});
+
+  const handleInput = (e) => {
+    setValues((prev) => ({ ...prev, [e.target.name]: [e.target.value] }));
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    setErrors(Validation(values));
+  };
   return (
     <div className="d-flex justify-content-center align-items-center bg-primary vh-100">
       <div className="bg-white p-3 rounded w-25">
         <h2>Sign Up</h2>
-        <form action="">
+        <form action="" onSubmit={handleSubmit}>
           <div className="mb-3">
             <label htmlFor="first name">
               <strong>First Name</strong>
@@ -14,6 +33,8 @@ const SignUp = () => {
             <input
               type="text"
               placeholder="Enter First Name"
+              name="firstName"
+              onChange={handleInput}
               className="form-control rounded-1"
             />
           </div>
@@ -25,6 +46,8 @@ const SignUp = () => {
             <input
               type="text"
               placeholder="Enter Last Name"
+              name="lastName"
+              onChange={handleInput}
               className="form-control rounded-1"
             />
           </div>
@@ -36,19 +59,26 @@ const SignUp = () => {
             <input
               type="text"
               placeholder="Enter User Name"
+              name="userName"
+              onChange={handleInput}
               className="form-control rounded-1"
             />
           </div>
 
           <div className="mb-3">
             <label htmlFor="email">
-              <strong>Password</strong>
+              <strong>Email</strong>
             </label>
             <input
-              type="password"
+              type="email"
               placeholder="Enter Password"
+              name="email"
+              onChange={handleInput}
               className="form-control rounded-1"
             />
+            {errors.email && (
+              <span className="text-danger">{errors.email}</span>
+            )}
           </div>
 
           <div className="mb-3">
@@ -58,11 +88,15 @@ const SignUp = () => {
             <input
               type="password"
               placeholder="Enter Password"
+              name="password"
+              onChange={handleInput}
               className="form-control rounded-1"
             />
           </div>
 
-          <button className="btn btn-success w-100 rounded-1">Sign Up</button>
+          <button className="btn btn-success w-100 rounded-1" type="submit">
+            Sign Up
+          </button>
 
           <p>You agree to our terms and conditions</p>
           <Link
