b82f617454e1ccde05e5952f23d84696756ad8ef
francis <francismainakabutha@gmail.com>
2023-06-15 12:11:10 +0300

    cleaned up the App.js file and ProtectedRoute.jsx file to remove unnecessary comments

--- client/src/App.js
+++ client/src/App.js
@@ -1,38 +1,3 @@
-// import React from "react";
-// import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-// import Login from "./components/Login";
-// import SignUp from "./components/SignUp";
-// import Home from "./components/Home/Home";
-// import ProtectedRoute from "./components/ProtectedRoute";
-
-// const App = () => {
-
-//   const checkAuthentication = () => {
-//     const token = localStorage.getItem("token");
-//     return !!token; // Convert token presence to boolean value
-//   };
-
-//   const isAuthenticated = checkAuthentication();
-
-//   return (
-//     <Router>
-//       <Routes>
-//         <Route path="/" element={<Login />} />
-//         <Route path="/signup" element={<SignUp />} />
-//         <Route
-//           element={
-//             <ProtectedRoute isAuthenticated={isAuthenticated} element={Home} />
-//           }
-//         >
-//           <Route path="/home" exact element={<Home />} />
-//         </Route>
-//       </Routes>
-//     </Router>
-//   );
-// };
-
-// export default App;
-
 import React, { useState, useEffect } from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Login from "./components/Login";
